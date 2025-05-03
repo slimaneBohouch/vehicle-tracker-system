8e32778d5a0bc9f110bcda05381a2beb02475ff3
Zakaria12e <zakariael1212@gmail.com>
2025-05-03 14:54:55 +0100

    Refactor user registration: removed 'phone' and 'role' from request body. Updated package-lock.json to remove unused fsevents dependency.

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ const sendEmail = require('../Utils/sendEmail');
 // @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
-  const { name, email, password, phone, company, role } = req.body;
+  const { name, email, password, company } = req.body;
 
   // Create user
   const user = await User.create({
