5bf6b46b1db57ef14d4706ea33d4812433de5e52
Zakaria12e <zakariael1212@gmail.com>
2025-06-03 22:21:47 +0100

    Implement code changes to enhance functionality and improve performance

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,8 @@ const User = require('../models/User')
 
 exports.getAllUsers = async (req, res, next) => {
   try {
-    const users = await User.find().select('name email role status company lastActive createdAt')
+   
+    const users = await User.find({ _id: { $ne: req.user.id } }).select('name email role status company lastActive createdAt')
     res.status(200).json({ success: true, data: users })
   } catch (error) {
     next(error)
